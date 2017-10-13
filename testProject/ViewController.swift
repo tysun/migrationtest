cd9d84ac6423ea51c4ad0a1ca69a9ec516f41a2d
Tysun Edison <tysun.edison@icloud.com>
2017-10-12 23:42:26 -0700

    committing and pushing iOS ViewController changes to origin/master

--- testProject/ViewController.swift
+++ testProject/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // inserting stuff for testing Git
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
